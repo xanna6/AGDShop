61f192baf2b8c3768466d1afd2554cf596d03ffa
xanna6 <apiotrow96@gmail.com>
2024-02-09 15:04:07 +0100

    displaying table with products

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
     session_start();
     require_once "connect.php";
-
+    $conn = new mysqli($host, $db_user, $db_password, $db_name);
+    $result = $conn->query("SELECT *, product.id as product_id, category.name as category_name FROM product JOIN category ON product.category = category.id");
+    $products = array();
+    while($product = $result->fetch_assoc()) {
+        $products[] =   $product;                      
+    }
 ?>
 
 
@@ -21,6 +26,22 @@
                 <a href="products.php" class="active">Produkty</a>
             </div>
         </div>
+
+        <?php 
+        echo '<div class="product_table">';
+        echo '<table style="width:100%">';
+            foreach($products as $product) {
+                $price = $product['price']/100;
+                $product_id = $product['product_id'];
+                echo "<tr id=".$product_id.">";
+                echo '<td class="product_name">'.$product["category_name"]." ".$product["manufacturer"]." ".$product["serial_number"]."</td>";
+                echo "<td>".number_format($price, 2, ",", "")." zł</td>";
+                echo "<td><button class='cart_button' type='submit' name='add_to_cart' value={$product_id}>Dodaj do koszyka</button></td> ";
+                echo "</tr>";
+            }
+        echo "</table>";
+        echo "</div>"
+        ?>
         
     </body>
 </html>    
